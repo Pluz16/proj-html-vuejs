dc7b8de09d2c789dafb84974f611461cf1a7859f
Francesco Punta <puntafrancesco@gmail.com>
2023-03-13 15:11:23 +0100

    Import FA + template Header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+      <nav>
+        <div class="logo">
+          <a href="#">Logo</a>
+        </div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Menu</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
