54d00d01abda6cc1c3ae2acbac5d657a2c46eeee
Francesco Punta <puntafrancesco@gmail.com>
2023-03-13 15:31:10 +0100

    Template footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h4>About Us</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+          <div class="col-md-4">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Menu</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <h4>Contact Us</h4>
+            <ul>
+              <li><i class="fas fa-map-marker-alt"></i> Via Roma 123, 00100 Roma</li>
+              <li><i class="fas fa-phone"></i> 06 1234567</li>
+              <li><i class="fas fa-envelope"></i> info@pizzeria.com</li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+        <p class="text-center">© 2023 Pizzeria | All rights reserved</p>
+      </div>
+    </footer>
+  </template>
+
+  
